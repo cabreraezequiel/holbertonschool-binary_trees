780d1d707c0be34ed7b520265a011492af087e68
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-17 16:25:14 -0300

    more corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,10 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int height, left, right = 0;
+	int height, left, right = -1;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
