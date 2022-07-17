39868a6230de8a40113cf6658f846ac664946d39
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-07-17 19:48:49 -0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,14 +17,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->n = value;
-	new_node->parent = parent;
 	new_node->right = NULL;
 	new_node->left = NULL;
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		new_node->right->parent = new_node; }
+		parent->right->parent = new_node; }
 
+	new_node->parent = parent;
 	parent->right = new_node;
 	return (new_node);
 }
