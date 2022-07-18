42a1964c1bc66df2ea47f7b533c4d45188b591af
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-18 12:54:35 -0300

    Corrections on 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    if ((tree->left) && (tree->right))
+        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+    return (0);
+}
