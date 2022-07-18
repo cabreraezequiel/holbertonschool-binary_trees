c7fdb4a3c93a34de0e3e6a1136a609dc61a5e4b2
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-18 11:55:47 -0300

    added function to 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,3 +19,21 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	return (node + 1);
 }
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == 0 && node->right == 0)
+		return (1);
+
+	else
+		return (0);
+}
