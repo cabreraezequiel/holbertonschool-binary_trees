04e26d3bd8fc5fb3c14cbfdeb496baed8493cdce
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-18 14:28:05 -0700

    Changed 110 back

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -22,55 +22,6 @@ int isBSTUtil(const binary_tree_t *node, int min, int max)
 		isBSTUtil(node->right, node->n + 1, max));
 }
 
-
-/**
- * find_max_value - finds the maximum value in a binary tree
- * @root: pointer to the root node of the tree to check
- * Return: max value
- */
-
-int find_max_value(const binary_tree_t *root)
-{
-	int res, lres, rres;
-
-	if (root == NULL)
-		return (INT_MIN);
-
-	res = root->n;
-	lres = find_max_value(root->left);
-	rres = find_max_value(root->right);
-	if (lres > res)
-		res = lres;
-	if (rres > res)
-		res = rres;
-	return (res);
-}
-
-
-/**
- * find_min_value - finds minimum value in a binary tree
- * @root: pointer to the root node of the tree to check
- * Return: min value
- */
-
-int find_min_value(const binary_tree_t *root)
-{
-	int res, lres, rres;
-
-	if (root == NULL)
-		return (INT_MAX);
-
-	res = root->n;
-	lres = find_min_value(root->left);
-	rres = find_min_value(root->right);
-	if (lres < res)
-		res = lres;
-	if (rres < res)
-		res = rres;
-	return (res);
-}
-
-
 /**
  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  * @tree: pointer to the root node of the tree to check
@@ -79,17 +30,5 @@ int find_min_value(const binary_tree_t *root)
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (1);
-
-	if (tree->left != NULL && find_max_value(tree->left) > tree->n)
-		return (0);
-
-	if (tree->right != NULL && find_min_value(tree->right) < tree->n)
-		return (0);
-
-	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
-		return (0);
-
-	return (1);
+	return (isBSTUtil(tree, INT_MIN, INT_MAX));
 }
