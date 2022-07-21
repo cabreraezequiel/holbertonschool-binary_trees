9a50887c2d9481f281357780ae1b827500a3f755
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-20 19:10:40 -0700

    Cleaned up code

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,40 +1,23 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-
-int is_avl(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (1);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (abs(left_height - right_height) <= 1 && is_avl(tree->left) && is_avl(tree->right))
-		return (1);
-	return (0);
-}
-
-
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: height of the tree
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
  */
 
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL)
-		return (0);
-	if (tree->left == NULL)
-		return (0);
 
-	return (is_avl(tree));
+	if (abs(binary_tree_balance(tree)) <= 1 &&
+	    abs(binary_tree_balance(tree->right)) <= 1 &&
+	    abs(binary_tree_balance(tree->left) <= 1))
+		return (1);
+	return (0);
 }
-
 
 /**
  * binary_tree_height - measures the height of a binary tree
@@ -44,34 +27,38 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left, right;
+	int height, left, right = 0;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
-		return (0);
+	if (tree == NULL)
+		return (-1);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
 	if (left > right)
-		return (left + 1);
+		height = 1 + left;
 	else
-		return (right + 1);
+		height = 1 + right;
+
+	return (height);
 }
 
+
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (node == NULL)
+	int left_height, right_height;
+
+	if (tree == NULL)
 		return (0);
 
-	if (node->left == 0 && node->right == 0)
-		return (1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	else
-		return (0);
+	return (left_height - right_height);
 }
